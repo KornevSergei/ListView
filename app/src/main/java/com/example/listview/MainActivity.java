7cf20242e3891f0756343c518c404950b45c88b9
4Korn <4Kornev@gmail.com>
2020-10-19 18:28:27 +0300

    commit 1

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //связываем
         ListView listView = findViewById(R.id.listView);
+        ListView colorListView = findViewById(R.id.colorListView);
+
 
         //заполняем лист 20 элементами через цикл
         ArrayList<String> posts = new ArrayList<>();
@@ -26,11 +32,52 @@ public class MainActivity extends AppCompatActivity {
             Log.i("posts: ", posts.get(i));
         }
 
+        //заполняем лист цветами
+        ArrayList<String> colorArrayList = new ArrayList<>();
+        colorArrayList.add("Красный");
+        colorArrayList.add("Белый");
+        colorArrayList.add("Черный");
+        colorArrayList.add("Синий");
+        colorArrayList.add("Желтый");
+        colorArrayList.add("Фиолетовый");
+
+
         //связываем элементами из массива и выбираем вид отображения
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,posts);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, posts);
         //Устанавливаем адаптер для листвью
         listView.setAdapter(arrayAdapter);
 
+
+        //связываем элементами из массива и выбираем вид отображения
+        ArrayAdapter<String> colorArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, colorArrayList);
+        //Устанавливаем адаптер для листвью
+        colorListView.setAdapter(colorArrayAdapter);
+
+
+        //добавляем клик на элемент листвью и выводим тост
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                //делаем невидимым весь список
+//                parent.setVisibility(view.GONE);
+//                //делаем невидимым элемент
+//                view.setVisibility(view.GONE);
+
+
+                //выводим тост по клику
+                Toast.makeText(MainActivity.this, "Ууф!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //добавляем клик на элемент листвью и выводим номер позиции и название
+        colorListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Номер " + position + " - " + colorArrayList.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
